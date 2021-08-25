75d6e58e1c6cf6abfe4944aeecd35cc4de483f41
Ajay Singh <daltavida@gmail.com>
2021-08-25 19:57:00 +1030

    Add scoring and loss system

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -10,8 +11,22 @@ document.querySelector(".check").addEventListener("click", function () {
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "Correct guess! 🥳";
   } else if (guess > secretNumber) {
-    document.querySelector(".message").textContent = "Too high 📈";
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too high 📈";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost the game 🎇";
+      document.querySelector(".score").textContent = 0;
+    }
   } else if (guess < secretNumber) {
-    document.querySelector(".message").textContent = "Too low 📉";
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too low 📉";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost the game 🎇";
+      document.querySelector(".score").textContent = 0;
+    }
   }
 });
