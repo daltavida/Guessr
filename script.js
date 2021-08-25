cebff795c1ae4e7eaabf9128ada08fcb148c939d
Ajay Singh <daltavida@gmail.com>
2021-08-25 19:57:00 +1030

    Create logic for guessing number

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 "use strict";
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
   if (!guess) {
     document.querySelector(".message").textContent = "Not a valid number! 🚫";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "Correct guess! 🥳";
+  } else if (guess > secretNumber) {
+    document.querySelector(".message").textContent = "Too high 📈";
+  } else if (guess < secretNumber) {
+    document.querySelector(".message").textContent = "Too low 📉";
   }
 });
